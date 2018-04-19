3cffac213dbf0cd8bd1f4086cf53eed1ccfcf5a7
Rodri <rodrigo.villarroel.m@gmail.com>
2018-04-19 19:05:36 -0400

    Hay esta chicos :3 te amo samu XD

--- app/src/main/java/com/programacion3/gruposinfinesdelucro/app/RutinasActivity.java
+++ app/src/main/java/com/programacion3/gruposinfinesdelucro/app/RutinasActivity.java
@@ -1,4 +1,38 @@
 package com.programacion3.gruposinfinesdelucro.app;
 
-public class RutinasActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RutinasActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    String[] exercises,duration;
+    int[] img_res;
+    ArrayList<DataProvider> arrayList= new ArrayList<DataProvider>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rutinas);
+        recyclerView = (RecyclerView)findViewById(R.id.recycler);
+        /*
+        exercises = getResources().getStringArray(R.id);
+        duration = getResources().getStringArray(R.id);
+        * */
+        int i=0;
+        for (String exercise: exercises){
+            DataProvider dataProvider=new DataProvider(img_res[i],exercise,duration[i]);
+            arrayList.add(dataProvider);
+            i++;
+        }
+        adapter= new RecyclerAdapter(arrayList);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
 }
