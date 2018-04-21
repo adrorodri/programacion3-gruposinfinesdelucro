2bb73d40a84f22878d3831db559bb0f1cd60adf9
Rodrigo <rodrigo.villarroel.m@gmail.com>
2018-04-20 20:33:42 -0400

    :D

--- app/src/main/java/com/programacion3/gruposinfinesdelucro/app/RutinasActivity.java
+++ app/src/main/java/com/programacion3/gruposinfinesdelucro/app/RutinasActivity.java
@@ -1,13 +1,18 @@
 package com.programacion3.gruposinfinesdelucro.app;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
-public class RutinasActivity extends AppCompatActivity {
+public class RutinasActivity extends AppCompatActivity  {
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
     RecyclerView.LayoutManager layoutManager;
@@ -18,21 +23,15 @@ public class RutinasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rutinas);
-        recyclerView = (RecyclerView)findViewById(R.id.recycler);
-        /*
-        exercises = getResources().getStringArray(R.id);
-        duration = getResources().getStringArray(R.id);
-        * */
-        int i=0;
-        for (String exercise: exercises){
-            DataProvider dataProvider=new DataProvider(img_res[i],exercise,duration[i]);
-            arrayList.add(dataProvider);
-            i++;
-        }
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        DataProvider dataProvider= new DataProvider(R.drawable.bicep1,"curl","12 min");
+        arrayList.add(dataProvider);
         adapter= new RecyclerAdapter(arrayList);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
-    }
+        }
+
+
 }
