d0d6e8bad656b0c070190ddc03c7e09687af40e5
Rodrigo <rodrigo.villarroel.m@gmail.com>
2018-04-23 19:25:59 -0400

    :D

---
+++ app/src/main/java/com/programacion3/gruposinfinesdelucro/app/ExplainActivity.java
@@ -0,0 +1,11 @@
+package com.programacion3.gruposinfinesdelucro.app;
+
+import android.os.Bundle;
+
+public class ExplainActivity extends NavigationActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_explain);
+        super.onCreate(savedInstanceState);
+    }
+}
