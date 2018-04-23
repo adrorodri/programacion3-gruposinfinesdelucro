cbcf85442aa8330ab1849d69885448e44b223c11
Rodrigo <rodrigo.villarroel.m@gmail.com>
2018-04-23 19:36:37 -0400

    :D

--- app/src/main/java/com/programacion3/gruposinfinesdelucro/app/ExplainActivity.java
+++ app/src/main/java/com/programacion3/gruposinfinesdelucro/app/ExplainActivity.java
@@ -1,11 +1,17 @@
 package com.programacion3.gruposinfinesdelucro.app;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class ExplainActivity extends NavigationActivity {
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_explain);
         super.onCreate(savedInstanceState);
+        imageView = findViewById(R.id.ejercicio);
+        Bundle parametros = this.getIntent().getExtras();
+
     }
 }
