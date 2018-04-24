69933e377701dae69f70b55aed75858f8b55ae08
HP <camila.loayzab@gmail.com>
2018-04-24 14:55:52 -0400

    Verificación de usuario

--- app/src/main/java/com/programacion3/gruposinfinesdelucro/app/LogInActivity.java
+++ app/src/main/java/com/programacion3/gruposinfinesdelucro/app/LogInActivity.java
@@ -5,22 +5,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LogInActivity extends AppCompatActivity {
 
     Button logInButton;
+    EditText Usua,Contra;
+    String CheckUsua, CheckContra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        Usua = findViewById(R.id.Usua);
+        Contra = findViewById(R.id.Contra);
+
+
         logInButton = findViewById(R.id.logButton   );
         logInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LogInActivity.this, ActividadDiariaActivity.class);
-                startActivity(intent);
+
+                CheckContra = String.valueOf(Contra.getText());
+                CheckUsua = String.valueOf(Usua.getText());
+
+                if (CheckUsua.equals("jorger@gmail.com") && CheckContra.equals("Pass123")) {
+                    Intent intent = new Intent(LogInActivity.this, ActividadDiariaActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LogInActivity.this, "Usuario Inválido", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
