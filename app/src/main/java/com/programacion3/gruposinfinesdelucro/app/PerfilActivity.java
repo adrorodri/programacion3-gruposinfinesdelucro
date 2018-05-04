87cc8d947683961229815c464d90b5455da15522
Samuel <hsamueljackson4@gmail.com>
2018-05-03 21:38:15 -0400

    Jorge

--- app/src/main/java/com/programacion3/gruposinfinesdelucro/app/PerfilActivity.java
+++ app/src/main/java/com/programacion3/gruposinfinesdelucro/app/PerfilActivity.java
@@ -2,27 +2,38 @@ package com.programacion3.gruposinfinesdelucro.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
 public class PerfilActivity extends NavigationActivity {
     boolean edit = false;
-    User user = super.getUser();
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private FirebaseUser user = auth.getCurrentUser();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_perfil);
         super.onCreate(savedInstanceState);
-        EditText editNombre, editEdad, editMetas;
+        EditText editNombre, editMail, editMetas;
         editNombre = findViewById(R.id.nombrePerfil);
-        editEdad = findViewById(R.id.años_perfil);
+        editMail = findViewById(R.id.email_perfil);
         editMetas = findViewById(R.id.metas_perfil);
-        updateInformation(editNombre, editEdad, editMetas);
+        updateInformation(editNombre, editMail, editMetas);
         editNombre.setEnabled(false);
-        editEdad.setEnabled(false);
+        editMail.setEnabled(false);
         editMetas.setEnabled(false);
 
     }
@@ -32,45 +43,53 @@ public class PerfilActivity extends NavigationActivity {
         editButton = findViewById(R.id.editButton);
         EditText editNombre, editEdad, editMetas;
         editNombre = findViewById(R.id.nombrePerfil);
-        editEdad = findViewById(R.id.años_perfil);
+        //editEdad = findViewById(R.id.años_perfil);
         editMetas = findViewById(R.id.metas_perfil);
         if (!edit) {
             editNombre.setEnabled(true);
-            editEdad.setEnabled(true);
+            //editEdad.setEnabled(true);
             editMetas.setEnabled(true);
             editButton.setText("Listo");
             edit = true;
         } else {
             editNombre.setEnabled(false);
-            editEdad.setEnabled(false);
+            //editEdad.setEnabled(false);
             editMetas.setEnabled(false);
             editButton.setText("Edit");
-            updateUser(editNombre.getText().toString(),editEdad.getText().toString(),editMetas.getText().toString());
-            updateInformation(editNombre, editEdad, editMetas);
+            updateUser(editNombre);
+            //updateInformation(editNombre);
             edit = false;
         }
     }
 
-    private void updateUser(String nombre, String edad, String metas) {
-        user.setName(nombre);
-        int edad2 = Integer.parseInt(edad);
-        user.setEdad(edad2);
-        user.setMetas(metas);
-        super.setUser(this.user);
+    private void updateUser(EditText nombre) {
+        //TODO edad y metas
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(nombre.getText().toString())
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("PerfilActivity", "User profile updated.");
+                        } else {
+                            Log.d("PerfilActivity", "Couldn't update info");
+                        }
+                    }
+                });
     }
 
-    public void updateInformation(EditText editNombre, EditText editEdad, EditText editMetas) {
-        editNombre.setText(user.getName());
-        String years = String.valueOf(user.getEdad());
-        editEdad.setText(years);
-        editMetas.setText(user.getMetas());
+
+    public void updateInformation(EditText editNombre, EditText editMail, EditText editMetas) {
+        editNombre.setText(user.getDisplayName());
+        //String years = String.valueOf(user.getEdad());
+        //ditEdad.setText(years);
+        //editMetas.setText(user.getMetas());
+        editMail.setText(user.getEmail());
+
     }
 
-    public User getUser() {
-        return user;
-    }
 
-    public void setUser(User user) {
-        this.user = user;
-    }
 }
