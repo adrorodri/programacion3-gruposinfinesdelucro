4b336409bfd73030588526e6658f06d760ba3fba
Samuel <hsamueljackson4@gmail.com>
2018-05-06 18:23:06 -0400

    Jorge

--- app/src/main/java/com/programacion3/gruposinfinesdelucro/app/PerfilActivity.java
+++ app/src/main/java/com/programacion3/gruposinfinesdelucro/app/PerfilActivity.java
@@ -1,26 +1,37 @@
 package com.programacion3.gruposinfinesdelucro.app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApiNotAvailableException;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PerfilActivity extends NavigationActivity {
     boolean edit = false;
     private FirebaseAuth auth = FirebaseAuth.getInstance();
     private FirebaseUser user = auth.getCurrentUser();
+    private Button uplButton;
+    private StorageReference mStorage;
+    private static final int GALLERY_INTENT = 1;
+    private CircleImageView image;
 
 
     @Override
@@ -29,13 +40,48 @@ public class PerfilActivity extends NavigationActivity {
         super.onCreate(savedInstanceState);
         EditText editNombre, editMail, editMetas;
         editNombre = findViewById(R.id.nombrePerfil);
-        editMail = findViewById(R.id.email_perfil);
-        editMetas = findViewById(R.id.metas_perfil);
+        editMail = findViewById(R.id.pesol_perfil);
+        editMetas = findViewById(R.id.tiempo_perfil);
         updateInformation(editNombre, editMail, editMetas);
         editNombre.setEnabled(false);
         editMail.setEnabled(false);
         editMetas.setEnabled(false);
 
+        uplButton = findViewById(R.id.subirFotoButton);
+        image = findViewById(R.id.circle_profile_image);
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+
+
+        uplButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,GALLERY_INTENT);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==GALLERY_INTENT&&resultCode== RESULT_OK){
+            Uri uri = data.getData();
+            StorageReference filepath = mStorage.child("fotosDePerfil").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri descargarFoto = taskSnapshot.getDownloadUrl();
+                    Glide.with(PerfilActivity.this)
+                            .load(descargarFoto).fitCenter().centerCrop().into(image);
+
+
+                    Toast.makeText(PerfilActivity.this,"Se subió la foto",Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
     }
 
     public void onEditClick(View view) {
@@ -45,7 +91,7 @@ public class PerfilActivity extends NavigationActivity {
         EditText editNombre, editEdad, editMetas;
         editNombre = findViewById(R.id.nombrePerfil);
         //editEdad = findViewById(R.id.años_perfil);
-        editMetas = findViewById(R.id.metas_perfil);
+        editMetas = findViewById(R.id.tiempo_perfil);
         if (!edit) {
             editNombre.setEnabled(true);
             //editEdad.setEnabled(true);
@@ -92,6 +138,5 @@ public class PerfilActivity extends NavigationActivity {
         editMail.setText(user.getEmail());
 
     }
-
 
 }
