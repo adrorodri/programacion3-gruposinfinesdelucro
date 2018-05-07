db8ea119724e2da786289995406df92fbf7b6254
Rodrigo <rodrigo.villarroel.m@gmail.com>
2018-05-07 14:54:15 -0400

    :D

--- app/src/main/java/com/programacion3/gruposinfinesdelucro/app/activities/ShowAllExercises.java
+++ app/src/main/java/com/programacion3/gruposinfinesdelucro/app/activities/ShowAllExercises.java
@@ -1,19 +1,51 @@
 package com.programacion3.gruposinfinesdelucro.app.activities;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.programacion3.gruposinfinesdelucro.app.R;
+import com.programacion3.gruposinfinesdelucro.app.adapters.EjerciciosAdapter;
+import com.programacion3.gruposinfinesdelucro.app.classes.Exercise;
+
+import java.util.ArrayList;
 
 public class ShowAllExercises extends NavigationActivity {
-
-
+    private ArrayList<Exercise> exerciseArrayList= new ArrayList<Exercise>();
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_show_all_exercises);
         super.onCreate(savedInstanceState);
+        recyclerView = findViewById(R.id.recycler);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("exercises");
+        DatabaseReference userDatabase = database.getReference("exercises");
+
+        ValueEventListener exerciseListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Exercise exercise = snapshot.getValue(Exercise.class);
+                    exerciseArrayList.add(exercise);
+                    Log.d("imagen",exercise.getImagen() + "nombre " +
+                           exercise.getName());
+
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        userDatabase.addValueEventListener(exerciseListener);
     }
 }
